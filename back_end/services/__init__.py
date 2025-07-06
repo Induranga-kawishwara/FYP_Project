3c46b106c384a811cb75a34c6a03f4eda66db995
Induranga Kawishwara <108388842+Induranga-kawishwara@users.noreply.github.com>
2025-07-07 03:14:30 +0530

    updated

--- back_end/services/__init__.py
+++ back_end/services/__init__.py
@@ -5,11 +5,11 @@ __version__ = "1.0.0"
 
 from .google_maps_service import fetch_and_filter_shops_with_text
 from .review_service import predict_review_rating_with_explanations, generate_summary
-from .google_scraper import get_real_reviews_serpapi
+from .google_scraper import get_real_reviews_playwright
 
 __all__ = [
     "fetch_and_filter_shops_with_text",
     "predict_review_rating_with_explanations",
     "generate_summary",
-    "get_real_reviews_serpapi"
+    "get_real_reviews_playwright"
 ]
